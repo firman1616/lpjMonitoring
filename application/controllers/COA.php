478978abaea73f3dc903232e7276d3d7eaf1c883
firman1616 <firmanfirdaus542@gmail.com>
2025-10-12 11:23:49 +0700

    update packing list coa ajinomoto

--- application/controllers/COA.php
+++ application/controllers/COA.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class COA extends CI_Controller {
+class COA extends CI_Controller
+{
 
     public function __construct()
     {
@@ -11,28 +12,29 @@ class COA extends CI_Controller {
         //     redirect(base_url("login"));
         // }
         // $this->load->library('pdf');
-        $this->load->model('M_COA','coa');
+        $this->load->model('M_COA', 'coa');
     }
 
-	public function index()
-	{
+    public function index()
+    {
         $data = [
             'title' => 'CoA',
             'table_title' => 'COA List',
             'conten' => 'coa/index',
             'footer_js' => array('assets/js/coa.js')
         ];
-		$this->load->view('template/conten',$data);
-	}
+        $this->load->view('template/conten', $data);
+    }
 
     function tableCOA()
     {
         $data['coa'] = $this->coa->getCOA()->result();
 
-        echo json_encode($this->load->view('coa/coa-table',$data,false));
+        echo json_encode($this->load->view('coa/coa-table', $data, false));
     }
 
-    function cetak_coa($coa)  {
+    function cetak_coa($coa)
+    {
         $header_data = $this->coa->cetak_coa($coa)->result();
         $detail_data = $this->coa->det_coa($coa)->result();
         $name = !empty($header_data) && isset($header_data[0]->name) ? $header_data[0]->name : $coa;
@@ -48,7 +50,8 @@ class COA extends CI_Controller {
         $this->pdf->createPDF($html, $name);
     }
 
-    function cetak_coa_aji($coa)  {
+    function cetak_coa_aji($coa)
+    {
         $header_data = $this->coa->cetak_coa($coa)->result();
         $detail_data = $this->coa->det_coa($coa)->result();
         $lot_aji = $this->coa->lot_aji($coa)->result();
@@ -64,5 +67,54 @@ class COA extends CI_Controller {
         $this->load->library('pdf');
         $html = $this->load->view('coa/cetak_coa_aji', $data, true);
         $this->pdf->createPDF($html, $name);
+    }
+
+    public function cetak_pl_aji($coa)
+    {
+        $header_data = $this->coa->cetak_coa($coa)->result();
+        // $detail_data = $this->sph->det_sph($sph)->result();
+
+        $name = !empty($header_data) && isset($header_data[0]->name) ? $header_data[0]->name : $coa;
+
+        $data = [
+            'title' => $name,
+            'header_sph' => $header_data,
+            // 'det_sph' => $detail_data,
+        ];
+
+        // Render HTML dari view
+        $html = $this->load->view('coa/cetak_coa_aji2', $data, true);
+
+        // Inisialisasi mPDF
+        $mpdf = new \Mpdf\Mpdf([
+            'format' => 'A4-L',
+            'margin_top' => 28,
+            'margin_bottom' => 30,
+            // 'default_font' => 'Arial',
+        ]);
+
+        // $logoPath = base_url('assets/img/LPJ_halal.jpg');
+
+
+        $mpdf->SetHTMLHeader('
+             <div style="text-align: center; font-size: 20px; font-weight: bold;">
+                PACKING LIST MATERIAL
+            </div>
+        ');
+
+        // $mpdf->SetHTMLFooter('
+        //     <div style="text-align: center; font-size: 11px;">
+        //         <hr style="border: none; border-top: 1px solid #000; margin-bottom: 5px;">
+        //         Office: Komplek Pergudangan Sinar Gedangan B-06 Ds. Gemurung - Gedangan<br>
+        //         Phone: +62 31 - 99038048, 99038054, 99038064 • Fax: +62 31 - 8011489<br>
+        //         Email: sales@laprintjaya.com • Website: http://www.laprintjaya.com
+        //     </div>
+        // ');
+
+        // Tambahkan HTML ke PDF
+        $mpdf->WriteHTML($html);
+
+        // Output ke browser
+        $mpdf->Output("PL_" . $name . ".pdf", 'I'); // 'I' = Inline (di-browser), 'D' = Download
     }
 }
